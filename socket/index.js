64e8b21281cb83061085e264c26501733d8aee97
Domains18 <gibsonsgibson88@gmail.com>
2023-06-09 15:16:53 +0300

    socket configurations
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

--- socket/index.js
+++ socket/index.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const api = express()
 require('dotenv').config();
+const path = require('path');
+const http = require('http').Server(api);
+
+//attaching socket.io to the http server
+const io = require('socket.io')(http);
+
+
+
 
 
 api.get('/', (req, res) => { 
-    res.send('Hello World!')
+    res.sendFile(path.join(__dirname, '/source/index.html'));
 })
 
+//cretae a new connection
+io.on('connection', socket => {
+    console.log('new connection made');
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    })
+
+});
 
 
 
-
-api.listen(process.env.PORT, ()=>{ console.log(`server running on port ${process.env.PORT}`)})
+http.listen(process.env.PORT, ()=>{ console.log(`server running on port ${process.env.PORT}`)})
